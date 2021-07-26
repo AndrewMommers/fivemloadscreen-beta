4ba099cbf010fbd48c7197cdbe756e673291d55f
Andrew <55376938+GamingPanthers@users.noreply.github.com>
2021-07-27 02:49:55 +1000

    1.0.0
    Based Frame of everything.
    
    Next update will fix background image.

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Andrew Mommers'
+description 'Loading Screen for FiveM'
+version '1.0.0'
+
+loadscreen "html/index.html"
+
+files {
+    'html/index.html',
+    'html/main.css',
+    'html/main.js',
+    'mikhail-sharov-mb-ks-2-2k.jpg'
+}
